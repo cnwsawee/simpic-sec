ecd5b6c34b1846d15fc1becaacec104752783acf
Chinnawudh Sawee <xuicide@Chinnawudhs-MacBook-Air.local>
2018-02-27 13:00:56 +0700

    add final

--- server.js
+++ server.js
@@ -18,51 +18,74 @@ app.get('/console2', function(req, res){
 app.get('/semi', function(req, res){
   res.sendFile(__dirname + '/semi.html');
 });
+app.get('/consolefin', function(req, res){
+  res.sendFile(__dirname + '/console-final.html');
+});
+app.get('/scorefin', function(req, res){
+  res.sendFile(__dirname + '/scoreboard-final.html');
+});
 
 app.use(express.static('public'));
 var score=[];
 for(var i=0;i<8;i++) score[i]=0;
 var score2=[];
+var scorefin=[];
 for(var i=0;i<6;i++) score2[i]=0;
+for(var i=1;i<5;i++) scorefin[i]=0;
 io.on('connection', function(socket){
     console.log('a user connected');
 
     socket.on('update', function(data){
-     for(var i=1;i<8;i++){
-      score[i]+=data[i];
-    }
-    score[0]++;
-    if(score[0]==5 || score[0]==7){
-      var min=1000;
-      for(var j=1;j<8;j++){
-        if(score[j]<min && score[j]!=-1) min=score[j];
+       for(var i=1;i<8;i++){
+        score[i]+=data[i];
       }
-      var count=0;
-      for(var j=1;j<8;j++){
-        if(score[j]==min) count++;
-      }
-      if(count<3){
+      score[0]++;
+      if(score[0]==5 || score[0]==7){
+        var min=1000;
         for(var j=1;j<8;j++){
-          if(score[j]==min) score[j]=-1;
+          if(score[j]<min && score[j]!=-1) min=score[j];
+        }
+        var count=0;
+        for(var j=1;j<8;j++){
+          if(score[j]==min) count++;
+        }
+        if(count<3){
+          for(var j=1;j<8;j++){
+            if(score[j]==min) score[j]=-1;
+          }
         }
       }
-    }
-    io.emit('score',score);
-    console.log('working');
-    })
+      io.emit('score',score);
+      console.log('working');
+    });
+
     socket.on('clear', function(data){
      for(var i=0;i<8;i++) score[i]=0;
        io.emit('cleared',score);
-    })
+    });
+
     socket.on('semi', function(i,data){
       score2[i]+=data;
       io.emit('result',i,score2[i]);
-    })
+    });
+
+    socket.on('sendfin',function(data){
+      for(var i=1;i<5;i++){
+            if(data[i]==0) scorefin[i]=scorefin[i]*4/5;
+            if(data[i]==1) scorefin[i]+=20;
+          }
+      io.emit('scorefin',scorefin);
+    });
+
    socket.on('clear2', function(data){
      for(var i=0;i<6;i++) score2[i]=0;
        io.emit('cleared2',1);
-    }) 
+    });
 
+   socket.on('clearfin', function(data){
+     for(var i=1;i<5;i++) scorefin[i]=0;
+       io.emit('clearedfin',scorefin);
+    }); 
 });
 
 http.listen(3000, function(){
